d711ba9f883d2b52e5e271b0438c99e426924ae8
Jon Edwards <jkedwards@me.com>
2016-06-03 00:58:12 +0000

    initial commit

---
+++ pirate_bartender.py
@@ -0,0 +1,43 @@
+import random
+
+questions = {
+    "strong": "Do ye like yer drinks strong?",
+    "salty": "Do ye like it with a salty tang?",
+    "bitter": "Are ye a lubber who likes it bitter?",
+    "sweet": "Would ye like a bit of sweetness with yer poison?",
+    "fruity": "Are ye one for a fruity finish?",
+}
+
+ingredients = {
+    "strong": ["glug of rum", "slug of whisky", "splash of gin"],
+    "salty": ["olive on a stick", "salt-dusted rim", "rasher of bacon"],
+    "bitter": ["shake of bitters", "splash of tonic", "twist of lemon peel"],
+    "sweet": ["sugar cube", "spoonful of honey", "spash of cola"],
+    "fruity": ["slice of orange", "dash of cassis", "cherry on top"],
+}
+
+def taste_questions(questions):
+    tastes = {}
+    for taste in questions:
+        valid_answer = False
+        while not(valid_answer):
+            value = str(input(questions[taste]))
+            if (value == 'y') or (value == 'yes'):
+                tastes[taste] = True
+                valid_answer = True
+            elif (value == 'n') or (value == 'no'):
+                tastes[taste] = False
+                valid_answer = True
+    return tastes
+    
+def mix_drink(tastes, ingredients):
+    drink = []
+    for taste in tastes:
+        if tastes[taste]:
+            drink.append(random.choice(ingredients[taste]))
+    return drink
+                
+if __name__ == '__main__':
+    tastes = taste_questions(questions)
+    drink = mix_drink(tastes, ingredients)
+    print(drink)
