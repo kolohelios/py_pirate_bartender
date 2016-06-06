09ef4ff0f653bf33360d894f782774ccf522b8ac
Jon Edwards <jkedwards@me.com>
2016-06-06 00:22:13 +0000

    add stock system and fix typo in sweet ingredient list

--- pirate_bartender.py
+++ pirate_bartender.py
@@ -9,11 +9,31 @@ questions = {
 }
 
 ingredients = {
-    "strong": ["glug of rum", "slug of whisky", "splash of gin"],
-    "salty": ["olive on a stick", "salt-dusted rim", "rasher of bacon"],
-    "bitter": ["shake of bitters", "splash of tonic", "twist of lemon peel"],
-    "sweet": ["sugar cube", "spoonful of honey", "spash of cola"],
-    "fruity": ["slice of orange", "dash of cassis", "cherry on top"],
+    "strong": [
+        {"name": "glug of rum", "stock": 32},
+        {"name": "slug of whisky", "stock": 17},
+        {"name": "splash of gin", "stock": 12},
+    ],
+    "salty": [
+        {"name": "olive on a stick", "stock": 11},
+        {"name": "salt-dusted rim", "stock": 52},
+        {"name": "rasher of bacon", "stock": 5},
+    ],
+    "bitter": [
+        {"name": "shake of bitters", "stock": 15},
+        {"name": "splash of tonic", "stock": 3},
+        {"name": "twist of lemon peel", "stock": 74},
+    ],
+    "sweet": [
+        {"name": "sugar cube", "stock": 65},
+        {"name": "spoonful of honey", "stock": 135},
+        {"name": "splash of cola", "stock": 4},
+    ],
+    "fruity": [
+        {"name": "slice of orange", "stock": 12},
+        {"name": "dash of cassis", "stock": 6},
+        {"name": "cherry on top", "stock": 89},
+    ],
 }
 
 def taste_questions(questions):
@@ -34,12 +54,24 @@ def mix_drink(tastes, ingredients):
     drink = []
     for taste in tastes:
         if tastes[taste]:
-            drink.append(random.choice(ingredients[taste]))
+            stocked_ingredient = ''
+            while not(stocked_ingredient):
+                ingredient = random.choice(ingredients[taste])
+                if ingredient['stock'] > 0:
+                    stocked_ingredient = ingredient
+                    for ingredient_from_stock in ingredients[taste]:
+                        print(ingredient_from_stock)
+                        if ingredient['name'] == ingredient_from_stock['name']:
+                            ingredient_from_stock['stock'] -= 1
+                            print('There are', ingredient_from_stock['stock'], 'of', ingredient['name'], 'left.')
+                else:
+                    print('Order some more', ingredient, '!')
+            drink.append(stocked_ingredient)
     return drink
     
 def drink_name():
     adjectives = ['Salty', 'Sassy', 'Fluffy', 'Drowned', 'Scuttled', 'Crusty', 'Sandy']
-    nouns = ['Shrimp', 'Plank', 'Squid', 'Davy Jones', 'Whisker', 'Kraken', '']
+    nouns = ['Shrimp', 'Plank', 'Squid', 'Davy Jones', 'Whisker', 'Kraken']
     drink = random.choice(adjectives) + ' ' + random.choice(nouns)
     return drink
                 
@@ -47,14 +79,31 @@ if __name__ == '__main__':
     customers = {}
     name = ''
     while True:
-        name = ''
-        while (len(name) < 1):
-            name = input('What should I call ye? (or "empty" bar) :')
-        if name == 'empty':
-            exit()
-        elif not(name in customers.keys()):
-            customers[name] = taste_questions(questions)
-        drink = mix_drink(customers[name], ingredients)
-        print('Your ' + drink_name() + ' is made of:')
-        for ingredient in drink:
-            print(ingredient)
+        menu_selection = ''
+        while (menu_selection != 'g') and (menu_selection != 's') and (menu_selection != 'w'):
+            menu_selection = input('Bar tender, re(s)tock the bar, (g)reet ye customer, or (w)alk the plank? ').lower()
+            if menu_selection == 'g':
+                name = ''
+                while (len(name) < 1):
+                    name = input('What should I call ye? (or "empty" bar) :')
+                if name == 'empty':
+                    exit()
+                elif not(name in customers.keys()):
+                    customers[name] = taste_questions(questions)
+                drink = mix_drink(customers[name], ingredients)
+                print('Your ' + drink_name() + ' is made of:')
+                for ingredient in drink:
+                    print(ingredient['name'])
+            elif menu_selection == 's':
+                print('Here\'s all ye ingredients and ye stock levels:')
+                for taste in ingredients:
+                    for ingredient in ingredients[taste]:
+                        print(ingredient['name'], ':', ingredient['stock'])
+                ingredient_to_restock = input("Which thing do ye need more of? ")
+                for taste in ingredients:
+                    for ingredient in ingredients[taste]:
+                        if ingredient['name'] == ingredient_to_restock:
+                            ingredient['stock'] += 10
+                            print('10 more', ingredient['name'], 'added.')
+            elif menu_selection == 'w':
+                exit()
